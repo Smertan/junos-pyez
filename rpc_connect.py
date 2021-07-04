878ac3c8e6ac3f67ac3f8dc4ac3404f59d75fa27
Smertan <86419745+Smertan@users.noreply.github.com>
2021-07-04 16:22:55 +0100

    Create rpc_connect.py

---
+++ rpc_connect.py
@@ -0,0 +1,23 @@
+from jnpr.junos import Device
+from getpass import getpass
+import jnpr.junos.exception
+
+ipadd = input('Enter hostname or IP address: ')
+username = input('Enter username: ')
+password = getpass('Enter password: ')
+
+try:
+    with Device(host=ipadd, user=username, password=password) as dev:
+        print('Are we connected?', dev.connected)
+        software = dev.rpc.get_software_information()
+        host_name = software.findtext("host-name")
+        model = software.findtext("product-model")
+        version = software.findtext("package-information/comment")
+        print('*' * 70,
+              "Operational checks".center(70),
+              f"\n\nHost-name: {host_name}",
+              f"Version: {version}",
+              f"Model: {model}",
+              "*" * 70, "\t",sep="\n")
+except jnpr.junos.exception.ConnectAuthError as autherr:
+    print('Check username and password', autherr)
