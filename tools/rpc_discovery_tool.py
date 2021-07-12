36a5eb5dbdd8fa2a97f8410225162c525e4fe8a8
Smertan <86419745+Smertan@users.noreply.github.com>
2021-07-12 18:54:29 +0100

    Update rpc_discovery_tool.py

--- tools/rpc_discovery_tool.py
+++ tools/rpc_discovery_tool.py
@@ -11,13 +11,18 @@ try:
     with Device(host=ipadd, password=key_password) as dev:
         print('Are we connected?', dev.connected)
         try:
+            print("*" * 70, "\n")
             if sys.argv[1]:
-                print("*" * 70, "\n")
                 for arg in sys.argv[1:]:
                     sys_cmd = dev.display_xml_rpc(arg).tag.replace("-", "_")
                     print(arg, f"= {sys_cmd}", "\n" + "-" * 70)
                 print("\n\n" + "*" * 70)
-        except:
-            pass  
+        except IndexError:
+            print(
+"""
+No command entered, i.e.,
+
+python3.py rpc.discovery_tool.py 'show version'
+""")
 except jnpr.junos.exception.ConnectAuthError as autherr:
     print('Check key password', autherr)
