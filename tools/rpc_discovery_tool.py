8fe7f4b006c05aac7f108d7121ca9ec5ce9b667f
Smertan <86419745+Smertan@users.noreply.github.com>
2021-07-12 16:17:04 +0100

    Update rpc_discovery_tool.py

--- tools/rpc_discovery_tool.py
+++ tools/rpc_discovery_tool.py
@@ -1,1 +1,23 @@
+from jnpr.junos import Device
+from getpass import getpass
+import jnpr.junos.exception
+import sys
 
+
+ipadd = input('Enter hostname or IP address: ')
+key_password = getpass('Enter key password: ')
+
+try:
+    with Device(host=ipadd, password=key_password) as dev:
+        print('Are we connected?', dev.connected)
+        try:
+            if sys.argv[1]:
+                print("*" * 70, "\n")
+                for arg in sys.argv[1:]:
+                    sys_cmd = dev.display_xml_rpc(arg).tag.replace("-", "_")
+                    print(arg, f"= {sys_cmd}", "\n" + "-" * 70)
+                print("\n\n" + "*" * 70)
+        except:
+            pass  
+except jnpr.junos.exception.ConnectAuthError as autherr:
+    print('Check key password', autherr)
