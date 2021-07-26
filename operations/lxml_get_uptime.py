3fabe3ca2300abf6dbbd1003b4c2ae8f9aa9f07a
Smertan <86419745+Smertan@users.noreply.github.com>
2021-07-26 12:40:34 +0100

    Create lxml_get_uptime.py

---
+++ operations/lxml_get_uptime.py
@@ -0,0 +1,20 @@
+from jnpr.junos import Device
+from getpass import getpass
+import jnpr.junos.exception
+from lxml import etree
+
+
+ipadd = input('Enter hostname or IP address: ')
+key_password = getpass('Enter key password: ')
+
+try:
+    with Device(host=ipadd, password=key_password) as dev:
+        print('Are we connected?', dev.connected)
+        sys_uptime = dev.rpc.get_system_uptime_information(normalize=True)
+        uptime = sys_uptime.findtext("uptime-information/up-time")
+        seconds = sys_uptime.find("uptime-information/up-time").attrib['seconds']
+        print("\n" + repr(uptime))
+        print(f"Total in seconds = {seconds}")
+        print()
+except jnpr.junos.exception.ConnectAuthError as autherr:
+    print('Check key password', autherr)
